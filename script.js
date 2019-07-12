46dfa99573500dbf2dc5de8e59d2a3ea26bbe903
chanarpeterson <52455200+chanarpeterson@users.noreply.github.com>
2019-07-12 14:51:21 +0300

    Refactoring js and adding css styles

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 //COLOR BUTTON VARIABLES
 var colorButtons = document.querySelectorAll('.btn-colors');
 
-var arrColorButtons = [];
-
-var colorButton1 = document.querySelectorAll('.btn-colors')[0];
-var colorButton2 = document.querySelectorAll('.btn-colors')[1];
-var colorButton3 = document.querySelectorAll('.btn-colors')[2];
-var colorButton4 = document.querySelectorAll('.btn-colors')[3];
-var colorButton5 = document.querySelectorAll('.btn-colors')[4];
-var colorButton6 = document.querySelectorAll('.btn-colors')[5];
+var colorButton1 = colorButtons[0];
+var colorButton2 = colorButtons[1];
+var colorButton3 = colorButtons[2];
+var colorButton4 = colorButtons[3];
+var colorButton5 = colorButtons[4];
+var colorButton6 = colorButtons[5];
 
 //RGB DISPLAY VARIABLE
 var rgbDisplay = document.getElementById("rgbDisplay");
@@ -22,38 +20,40 @@ var modeEasy = document.getElementById("easy-mode");
 var modeHard = document.getElementById("hard-mode");
 
 
-//BUTTON RANDOMIZER VARIABLE
-var arrButton = [];
-
-for(var i = 0; i < colorButtons.length; i++){
-  var name = i;
-  arrColorButtons.push(name);
-}
-
+//BACKGROUND DISPLAY VARIABLE (Not in use - considering changing color to match correct rgb)
+var bodyBackground = document.querySelector("body");
 
 //GENERATE RANDOM RGB SEQUENCE FUNCTION
 function generateRandomRgb(){
-  var iRandom = Math.floor((Math.random() * colorButtons.length));
-  var r = Math.floor((Math.random() * 254) + 1);
-  var g = Math.floor((Math.random() * 254) + 1);
-  var b = Math.floor((Math.random() * 254) + 1);
-  var rgb = "rgb(" + r + ", " + g + ", " + b + ")";
-  rgbDisplay.textContent = rgb;
-  colorButtons[iRandom].style.backgroundColor = rgb;
+  var i = Math.floor((Math.random() * colorButtons.length));
+  var correctRgb = returnRgb();
+  rgbDisplay.textContent = correctRgb;
+  colorButtons[i].style.backgroundColor = correctRgb;
 }
+
+      //FIND A RANDOM NUMBER BETWEEN 0 - 255
+      function randomNumber(){
+        var randomNum = Math.floor((Math.random() * 255));
+        return randomNum;
+      }
+
+      //CONCATENATE A RANDOM RGB VALUE
+      function returnRgb(){
+        var r = randomNumber();
+        var g = randomNumber();
+        var b = randomNumber();
+        var rgb = "rgb(" + r + ", " + g + ", " + b + ")";
+        return rgb;
+      }
 
 //FILL THE BOARD WITH RANDOM COLORS, INCLUDING CORRECT RGB
 function fillBoard(){
   for (var i = 0; i < colorButtons.length; i++){
-    var r = Math.floor((Math.random() * 254) + 1);
-    var g = Math.floor((Math.random() * 254) + 1);
-    var b = Math.floor((Math.random() * 254) + 1);
-    var rgb = "rgb(" + r + ", " + g + ", " + b + ")";
-    colorButtons[i].style.backgroundColor = rgb;
+    var randomColor = returnRgb();
+    colorButtons[i].style.backgroundColor = randomColor;
   }
   generateRandomRgb();
 }
-
 
 
 //ON LOAD FILL BOARD AND GENERATE RANDOM RGB
@@ -61,26 +61,16 @@ fillBoard();
 
 //NEW COLORS: FILLS BOARD AND GENERATES RANDOM RGB
 newColors.addEventListener("click", function(){
-  fillBoard();
+  if(rowBottom.style.display === "none"){
+    playEasy();
+  } else {
+    fillBoard();
+  }
   feedback.textContent = "Have a guess!";
 });
 
 
 //CLICK CORRECT BUTTON AND WIN
-function feedbackTrue(){
-  feedback.textContent = "Correct!";
-  for (var i = 0; i < colorButtons.length; i++){
-    if (colorButtons[i].style.backgroundColor !== rgbDisplay.innerHTML) {
-      colorButtons[i].style.backgroundColor = "white";
-    }
-  }
-}
-
-function feedbackFalse(){
-  feedback.textContent = "Try again";
-}
-
-
 function correctGuess(){
   if (this.style.backgroundColor === rgbDisplay.innerHTML){
     feedbackTrue();
@@ -91,47 +81,63 @@ function correctGuess(){
 }
 
 
-colorButton1.addEventListener("click", correctGuess);
-colorButton2.addEventListener("click", correctGuess);
-colorButton3.addEventListener("click", correctGuess);
-colorButton4.addEventListener("click", correctGuess);
-colorButton5.addEventListener("click", correctGuess);
-colorButton6.addEventListener("click", correctGuess);
+      //CHANGE FEEDBACK IN CASE OF CORRECT GUESS
+      function feedbackTrue(){
+        feedback.textContent = "Correct!";
+        for (var i = 0; i < colorButtons.length; i++){
+          if (colorButtons[i].style.backgroundColor !== rgbDisplay.innerHTML) {
+            colorButtons[i].style.backgroundColor = "white";
+          }
+        }
+      }
+
+
+      //CHANGE FEEDBACK IN CASE OF INCORRECT GUESS
+      function feedbackFalse(){
+        feedback.textContent = "Try again";
+      }
+
+
+      //EVENT LISTENERS FOR COLOR BUTTONS
+      colorButton1.addEventListener("click", correctGuess);
+      colorButton2.addEventListener("click", correctGuess);
+      colorButton3.addEventListener("click", correctGuess);
+      colorButton4.addEventListener("click", correctGuess);
+      colorButton5.addEventListener("click", correctGuess);
+      colorButton6.addEventListener("click", correctGuess);
 
 
 //EASY MODE
 var rowBottom = document.querySelector(".row-bottom");
 
-modeEasy.addEventListener("click", function(){
+modeEasy.addEventListener("click", playEasy);
+
+    //PLAY EASY FUNCTION
+    function playEasy(){
       rowBottom.style.display = "none";
-    //GENERATE RANDOM RGB SEQUENCE FUNCTION
-    function generateRandomRgbEasy(){
-      var iRandom = Math.floor((Math.random() * 3));
-      var r = Math.floor((Math.random() * 254) + 1);
-      var g = Math.floor((Math.random() * 254) + 1);
-      var b = Math.floor((Math.random() * 254) + 1);
-      var rgb = "rgb(" + r + ", " + g + ", " + b + ")";
-      rgbDisplay.textContent = rgb;
-      colorButtons[iRandom].style.backgroundColor = rgb;
-    }
-
-    //FILL THE BOARD WITH RANDOM COLORS, INCLUDING CORRECT RGB
-    function fillBoardEasy(){
-      for (var i = 0; i < colorButtons.length; i++){
-        var r = Math.floor((Math.random() * 254) + 1);
-        var g = Math.floor((Math.random() * 254) + 1);
-        var b = Math.floor((Math.random() * 254) + 1);
-        var rgb = "rgb(" + r + ", " + g + ", " + b + ")";
-        colorButtons[i].style.backgroundColor = rgb;
+      //MAKE SURE CORRECT RGB IS IN THE TOP THREE BUTTONS
+      function generateRandomRgbEasy(){
+        var i = Math.floor((Math.random() * 3));
+        var correctRgb = returnRgb();
+        rgbDisplay.textContent = correctRgb;
+        colorButtons[i].style.backgroundColor = correctRgb;
       }
-      generateRandomRgbEasy();
+
+
+      //FILL THE BOARD WITH RANDOM COLORS, INCLUDING CORRECT RGB
+      function fillBoardEasy(){
+        for (var i = 0; i < colorButtons.length; i++){
+          var randomColor = returnRgb();
+          colorButtons[i].style.backgroundColor = randomColor;
+        }
+        generateRandomRgbEasy();
+      }
+      fillBoardEasy();
     }
-    fillBoardEasy();
-  });
 
 
-  //HARD MODE
-  modeHard.addEventListener("click", function(){
+//HARD MODE
+modeHard.addEventListener("click", function(){
   rowBottom.style.display = "";
   fillBoard();
-  });
+});
